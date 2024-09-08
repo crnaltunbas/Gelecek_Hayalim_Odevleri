febe1d0e8c3b5822e37d64b5f73e99d5decdbb07
Ceren Altunbas <crnaltunbas35@gmail.com>
2024-09-08 16:13:30 +0300

    Hex kodu ile kullanılmak üzere renk paletimiz oluşturuldu .

---
+++ lib/assets/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class WhatCurrencyAppColors {
+  static const Color primaryColor =
+  Color(0xFF006DAA); // AppBar da ve eklencek butonlarda kullanılacak
+  static const Color secondColor =
+  Color(0xFF0353A4); // AppBar harici kısımlarda kullanılacak
+  static const Color thirdColor =
+  Color(0xFF061A40); // genelde yazı ve icon rengi olacak
+}
+// Bu dosyada hex kodu ile renkleri seçebilmek için kullanacağımız renkler eklendi.
