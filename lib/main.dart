be7ddd436aeae9c1760620cf0277336853312e9c
Ceren Altunbas <crnaltunbas35@gmail.com>
2024-09-08 21:46:01 +0300

    Temel ekran design yapıldı navigationBar eklenecek

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'lib/assets/colors.dart';
+import 'package:what_currency_app/assets/colors.dart';
+
 void main() {
   runApp(const WhatCurrencyApp());
 }
@@ -10,11 +11,33 @@ class WhatCurrencyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: AppBar(
-        title: const Text(
-            "What Currency",
-        style: TextStyle(fontSize: 20,color: WhatCurrencyAppColors.thirdColor),),
+      home: Scaffold(
+        backgroundColor:
+            WhatCurrencyAppColors.secondColor, //Arka plan rengi eklendi
+        appBar: AppBar(
+            elevation: 20, // AppBar'a gölge eklemiş olduk.
+            backgroundColor: WhatCurrencyAppColors
+                .primaryColor, //AppBar'ın arka plan rengi eklendi
+            flexibleSpace: const Padding(
+              //flexibleSpace ile center widgetı sayesinde texti ortaladık padding sayesinde de aşağı indirdik.
+              padding: EdgeInsets.only(top: 35.0),
+              child: Center(
+                child: Text(
+                  "What Currency",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Color(0xFF99DAFF), //Burada neden colors sınıfından ismi kabul ettmediğini anlayamadım.
+                    fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.normal,
+                  ),
+                ),
+              ),
+            )),
       ),
     );
   }
 }
+
+
+
+
